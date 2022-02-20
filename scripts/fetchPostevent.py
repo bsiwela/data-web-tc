f6d07fd6e4f21bc91aae8c05728bf069491b4082
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-02-20 08:39:37 +0000

    Add files via upload

--- scripts/fetchPostevent.py
+++ scripts/fetchPostevent.py
@@ -1,15 +1,13 @@
 import os
-import shutil
-import requests
-import pandas as pd
+import json
+import re
 import geopandas as gpd
 import numpy as np
 import shapely.geometry as geom
 import nczip2geojson as nc
-from io import StringIO
 from urllib.parse import urlparse
-from bs4 import BeautifulSoup
 from ncgzip2losses import calculateLosses
+from utils import listFilesUrl, fetchUrl
 
 import xarray as xr
 from zipfile import ZipFile
@@ -18,11 +16,11 @@ url = 'https://www.kacportal.com/portal/kacs3/arc/mpres_data/postevent/'
 username = os.environ['KAC_USERNAME']
 password = os.environ['KAC_PASSWORD']
 
-
-def listFilesUrl(url, username, password, ext=''):
-    page = requests.get(url, auth=(username, password)).text
-    soup = BeautifulSoup(page, 'html.parser')
-    return [url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+with open('index.json', 'r') as f:
+    index = json.load(f)
+# files_tc_realtime = [os.path.splitext(os.path.split(p)[1])[0] for p in index['tc_realtime']]
+# files_tc_realtime_url = [os.path.splitext(os.path.split(url)[1])[0] for url in data[data.columns[-1]].values]
+# files_to_remove = [file for file in files_tc_realtime if file not in files_tc_realtime_url]
 
 list_subfolder = ['taos_swio30s_ofcl_windwater_nc', 'taos_swio30s_ofcl_windwater_shp']
 
@@ -45,70 +43,71 @@ for subfolder in list_subfolder:
 
     file_list = listFilesUrl(url_subfolder, username, password, ext='.zip')
 
+    try:
+        local_storm_files = [f'SH{re.search("SH(.+?)_", os.path.splitext(os.path.split(file)[1])[0]).group(1)}' for file in index['mpres_data'] if f'postevent/{subfolder}' in file and file.endswith('geojson')]
+    except:
+        local_storm_files = []  # in the case of shp files
+
     for url_file in file_list:
         filename = os.path.basename(urlparse(url_file).path)
 
-        print(f'Dealing with {filename}')
-        r = requests.get(url_file, auth=(username, password))
+        common_list = [storm_name for storm_name in local_storm_files if storm_name in filename]  # check if already processed
 
-        # writing the file locally
-        if r.status_code == 200:
-            with open(filename, 'wb') as out:
-                for bits in r.iter_content():
-                    out.write(bits)
+        if len(common_list) == 0:
+            downloaded = fetchUrl(url_file, username, password)
 
-        print(f'\tCopied local file {filename}')
+            if downloaded:
 
-        if subfolder == 'taos_swio30s_ofcl_windwater_nc':
-            with ZipFile(filename, 'r') as zipObject:
-                zippedFiles = zipObject.namelist()
-                for zippedFile in zippedFiles:
-                    if zippedFile.endswith('.nc'): # only keep past and not fcst
-                        zipObject.extract(zippedFile) #, os.path.split(zipfile)[0])
-                        #zippedFile = f'{os.path.split(zipfile)[0]}/{zippedFile}'
-                        nc.nc2geojson(zippedFile)
-                    else:
-                        print(f'Unknown file format for: {zippedFile}')
+                if subfolder == 'taos_swio30s_ofcl_windwater_nc':
+                    with ZipFile(filename, 'r') as zipObject:
+                        zippedFiles = zipObject.namelist()
+                        for zippedFile in zippedFiles:
+                            if zippedFile.endswith('.nc'): # only keep past and not fcst
+                                zipObject.extract(zippedFile) #, os.path.split(zipfile)[0])
+                                #zippedFile = f'{os.path.split(zipfile)[0]}/{zippedFile}'
+                                nc.nc2geojson(zippedFile)
+                            else:
+                                print(f'Unknown file format for: {zippedFile}')
 
-                    # running loss generation
-                    calculateLosses(storm_file=zippedFile, exp_file=os.path.join(root_root, 'arc_exposure.gzip'),
-                                        adm_file=os.path.join(root_root, 'adm2_full_precision.json'),
-                                        mapping_file=os.path.join(root_root, 'mapping.gzip'), split=False,
-                                        geojson=False)
-                    os.remove(zippedFile)
+                            # running loss generation
+                            calculateLosses(storm_file=zippedFile, exp_file=os.path.join(root_root, 'arc_exposure.gzip'),
+                                                adm_file=os.path.join(root_root, 'adm2_full_precision.json'),
+                                                mapping_file=os.path.join(root_root, 'mapping.gzip'), split=False,
+                                                geojson=False)
+                            os.remove(zippedFile)
 
-        elif subfolder == 'taos_swio30s_ofcl_windwater_shp':
-            nc.zip2geojson(filename)
+                elif subfolder == 'taos_swio30s_ofcl_windwater_shp':
+                    nc.zip2geojson(filename)
 
-            gdf = gpd.read_file(filename)
+                    gdf = gpd.read_file(filename)
 
-            # looping through storms
-            for storm_id in gdf.ATCFID.unique():
-                last_lon = None
-                last_lat = None
-                if 'TECH' in gdf.columns:
-                    for tech in gdf['TECH'].unique():
-                        gdf_storm = gdf[(gdf.ATCFID == storm_id) & (gdf.TECH == tech)].sort_values(by=['DTG'])
-                        lons = gdf_storm['LON'].values
-                        lats = gdf_storm['LAT'].values
-                        if tech == 'FCST' and last_lon and last_lat:
-                            lons = np.insert(lons, 0, last_lon)
-                            lats = np.insert(lats, 0, last_lat)
-                        if len(lons) > 1 and len(lats) > 1:
-                            lineString = geom.LineString([(lon, lat) for lon, lat in zip(lons, lats)])
-                            row = gdf_storm.iloc[-1]
-                            if tech == 'TRAK':
-                                last_lon = row.LON
-                                last_lat = row.LAT
-                            row.geometry = lineString
-                            gdf = gdf.append(row)
+                    # looping through storms
+                    for storm_id in gdf.ATCFID.unique():
+                        last_lon = None
+                        last_lat = None
+                        if 'TECH' in gdf.columns:
+                            for tech in gdf['TECH'].unique():
+                                gdf_storm = gdf[(gdf.ATCFID == storm_id) & (gdf.TECH == tech)].sort_values(by=['DTG'])
+                                lons = gdf_storm['LON'].values
+                                lats = gdf_storm['LAT'].values
+                                if tech == 'FCST' and last_lon and last_lat:
+                                    lons = np.insert(lons, 0, last_lon)
+                                    lats = np.insert(lats, 0, last_lat)
+                                if len(lons) > 1 and len(lats) > 1:
+                                    lineString = geom.LineString([(lon, lat) for lon, lat in zip(lons, lats)])
+                                    row = gdf_storm.iloc[-1]
+                                    if tech == 'TRAK':
+                                        last_lon = row.LON
+                                        last_lat = row.LAT
+                                    row.geometry = lineString
+                                    gdf = gdf.append(row)
 
 
-            geojsonFilePath = f'{os.path.splitext(filename)[0]}.geojson'
-            gdf.to_file(geojsonFilePath, driver='GeoJSON')
+                    geojsonFilePath = f'{os.path.splitext(filename)[0]}.geojson'
+                    gdf.to_file(geojsonFilePath, driver='GeoJSON')
 
-        # removing zip and nc files
-        os.remove(filename)
+                # removing zip and nc files
+                os.remove(filename)
 
 
 
