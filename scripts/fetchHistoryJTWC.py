ca129c1b0c34f6131ebbd1207ea27bae0f3e4f27
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-02-24 21:07:18 +0000

    Update fetchHistoryJTWC.py

--- scripts/fetchHistoryJTWC.py
+++ scripts/fetchHistoryJTWC.py
@@ -45,60 +45,10 @@ def fetchHistoryJTWC(url, adm_file, mapping_file, geojson=False):
 
         os.chdir(year_dir)
 
-        for url_file in file_list_zip:
-
-            filename = os.path.basename(urlparse(url_file).path)
-            print(f'Dealing with {filename}')
-            r = requests.get(url_file, auth=(username, password))
-
-            # writing the file locally
-            if r.status_code == 200:
-                with open(filename, 'wb') as out:
-                    for bits in r.iter_content():
-                        out.write(bits)
-
-            with nc.ZipFile(filename, 'r') as zipObject:
-                zippedFiles = zipObject.namelist()
-                for zippedFile in zippedFiles:
-                    if zippedFile.endswith('.csv'):
-                        zipObject.extract(zippedFile, '')  # f'{os.path.splitext(os.path.basename(filename))[0]}.csv'
-
-                        df = pd.read_csv(zippedFile)
-                        if not(df.empty):
-                            df = df.merge(df['exposure_id'].apply(lambda s: pd.Series(
-                                {f'adm{i}_code': int(mapping.loc[s, f'adm{i}_code']) for i in range(3)})), left_index=True,
-                                     right_index=True)
-                            df_groupby = df.groupby(by=['adm0_code','adm1_code','adm2_code'], as_index=False).sum()
-                            df_merge = pd.merge(df_groupby, adm_df.dissolve(by='ADM2_CODE', as_index=False), left_on='adm2_code', right_on='ADM2_CODE')
-
-                            cols2keep = ['ADM0_NAME','ADM1_NAME','ADM2_NAME','ADM0_CODE','ADM1_CODE','ADM2_CODE','population','tloss','geometry']
-                            df_merge = df_merge[cols2keep]
-                            df_merge.rename(columns={'tloss': 'loss', 'ADM0_NAME': 'adm0_name', 'ADM1_NAME': 'adm1_name', 'ADM2_NAME': 'adm2_name', 'ADM0_CODE': 'adm0_code', 'ADM1_CODE': 'adm1_code', 'ADM2_CODE': 'adm2_code'}, inplace=True)
-
-                            if geojson:
-                                df_final = gpd.GeoDataFrame(df_merge)
-                            else:
-                                df_final = df_merge.drop(columns='geometry')
-
-                            jsonFileBase = f'{os.path.splitext(os.path.basename(filename))[0]}_losses_adm'
-
-                            for i in range(3):
-                                jsonFile = f'{jsonFileBase}{i}.{"geo" if geojson else ""}json'
-                                adm_group_list = [f'adm{j}_name' for j in range(i + 1)]
-                                if geojson:
-                                    gdf_adm = df_final.dissolve(by=adm_group_list, aggfunc='sum', as_index=False)
-                                    gdf_adm.to_file(jsonFile, driver='GeoJSON')
-                                    densify(jsonFile)
-                                else:
-                                    df_final.groupby(adm_group_list, as_index=False).sum().to_json(jsonFile, orient='records')
-
-                        os.remove(filename)
-                        os.remove(zippedFile)
-
-        # for url_file in file_list_nc:
+        # for url_file in file_list_zip:
         #
         #     filename = os.path.basename(urlparse(url_file).path)
-        #     print(f'Dealing with {filename} ... ', end='')
+        #     print(f'Dealing with {filename}')
         #     r = requests.get(url_file, auth=(username, password))
         #
         #     # writing the file locally
@@ -106,41 +56,63 @@ def fetchHistoryJTWC(url, adm_file, mapping_file, geojson=False):
         #         with open(filename, 'wb') as out:
         #             for bits in r.iter_content():
         #                 out.write(bits)
-        #     print('received')
         #
-        #     # converting it to geojson
-        #     nc.nc2geojson(filename, N=50)
+        #     with nc.ZipFile(filename, 'r') as zipObject:
+        #         zippedFiles = zipObject.namelist()
+        #         for zippedFile in zippedFiles:
+        #             if zippedFile.endswith('.csv'):
+        #                 zipObject.extract(zippedFile, '')  # f'{os.path.splitext(os.path.basename(filename))[0]}.csv'
         #
-        #     # adding lineString for storm shapefile
-        #     if 'storm' in filename:
-        #         gdf = gpd.read_file(filename)
+        #                 df = pd.read_csv(zippedFile)
+        #                 if not(df.empty):
+        #                     df = df.merge(df['exposure_id'].apply(lambda s: pd.Series(
+        #                         {f'adm{i}_code': int(mapping.loc[s, f'adm{i}_code']) for i in range(3)})), left_index=True,
+        #                              right_index=True)
+        #                     df_groupby = df.groupby(by=['adm0_code','adm1_code','adm2_code'], as_index=False).sum()
+        #                     df_merge = pd.merge(df_groupby, adm_df.dissolve(by='ADM2_CODE', as_index=False), left_on='adm2_code', right_on='ADM2_CODE')
         #
-        #         # looping through storms
-        #         for storm_id in gdf.ATCFID.unique():
-        #             last_lon = None
-        #             last_lat = None
-        #             for tech in gdf.TECH.unique():
-        #                 gdf_storm = gdf[(gdf.ATCFID == storm_id) & (gdf.TECH == tech)].sort_values(by=['DTG'])
-        #                 lons = gdf_storm['LON'].values
-        #                 lats = gdf_storm['LAT'].values
-        #                 if tech == 'FCST' and last_lon and last_lat:
-        #                     lons = np.insert(lons, 0, last_lon)
-        #                     lats = np.insert(lats, 0, last_lat)
-        #                 if len(lons) > 1 and len(lats) > 1:
-        #                     lineString = geom.LineString([(lon, lat) for lon, lat in zip(lons, lats)])
-        #                     row = gdf_storm.iloc[-1]
-        #                     if tech == 'TRAK':
-        #                         last_lon = row.LON
-        #                         last_lat = row.LAT
-        #                     row.geometry = lineString
-        #                     gdf = gdf.append(row)
+        #                     cols2keep = ['ADM0_NAME','ADM1_NAME','ADM2_NAME','ADM0_CODE','ADM1_CODE','ADM2_CODE','population','tloss','geometry']
+        #                     df_merge = df_merge[cols2keep]
+        #                     df_merge.rename(columns={'tloss': 'loss', 'ADM0_NAME': 'adm0_name', 'ADM1_NAME': 'adm1_name', 'ADM2_NAME': 'adm2_name', 'ADM0_CODE': 'adm0_code', 'ADM1_CODE': 'adm1_code', 'ADM2_CODE': 'adm2_code'}, inplace=True)
         #
+        #                     if geojson:
+        #                         df_final = gpd.GeoDataFrame(df_merge)
+        #                     else:
+        #                         df_final = df_merge.drop(columns='geometry')
         #
-        #         geojsonFilePath = f'{os.path.splitext(filename)[0]}.geojson'
-        #         gdf.to_file(geojsonFilePath, driver='GeoJSON')
+        #                     jsonFileBase = f'{os.path.splitext(os.path.basename(filename))[0]}_losses_adm'
         #
-        #     # removing nc file
-        #     os.remove(filename)
+        #                     for i in range(3):
+        #                         jsonFile = f'{jsonFileBase}{i}.{"geo" if geojson else ""}json'
+        #                         adm_group_list = [f'adm{j}_{k}' for j in range(i + 1) for k in ['name', 'code']]
+        #                         if geojson:
+        #                             gdf_adm = df_final.dissolve(by=adm_group_list, aggfunc='sum', as_index=False)
+        #                             gdf_adm.to_file(jsonFile, driver='GeoJSON')
+        #                             densify(jsonFile)
+        #                         else:
+        #                             df_final.groupby(adm_group_list, as_index=False)['population','loss'].sum().to_json(jsonFile, orient='records')
+        #
+        #                 os.remove(filename)
+        #                 os.remove(zippedFile)
+
+        for url_file in file_list_nc:
+
+            filename = os.path.basename(urlparse(url_file).path)
+            print(f'Dealing with {filename} ... ', end='')
+            r = requests.get(url_file, auth=(username, password))
+
+            # writing the file locally
+            if r.status_code == 200:
+                with open(filename, 'wb') as out:
+                    for bits in r.iter_content():
+                        out.write(bits)
+            print('received')
+
+            # converting it to geojson
+            nc.nc2geojson(filename, N=50)
+
+            # removing nc file
+            os.remove(filename)
 
 
 
