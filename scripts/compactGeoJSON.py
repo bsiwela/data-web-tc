358b2d18c4fa6d2bec3f9619c5110739323bc581
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-02-26 13:10:08 +0000

    Update compactGeoJSON.py

--- scripts/compactGeoJSON.py
+++ scripts/compactGeoJSON.py
@@ -4,14 +4,14 @@ import argparse
 import numpy as np
 
 
-def densify(geojsonFilePath):
+def densify(geojsonFilePath, decimals=2):
 
     with open(geojsonFilePath, 'r') as f:
         data = json.load(f)
 
     for i, p in enumerate(data['features']):
         try:
-            p['geometry']['coordinates'] = [np.round(np.array(poly), 4).tolist() for poly in p['geometry']['coordinates']]
+            p['geometry']['coordinates'] = [np.round(np.array(poly), decimals=decimals).tolist() for poly in p['geometry']['coordinates']]
         except:
             print(f'{i}: Could not be transformed ')
             continue
@@ -24,6 +24,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Arguments to be passed to the script')
     parser.add_argument('-g', '--geojson', type=str, help='Path to GeoJSON file', default=None, dest='geojsonFilePath')
+    parser.add_argument('-d', '--decimals', type=int, help='Decimal precision', default=4, dest='decimals')
     args = parser.parse_args()
 
-    densify(geojsonFilePath=args.geojsonFilePath)
+    densify(geojsonFilePath=args.geojsonFilePath, decimals=args.decimals)
