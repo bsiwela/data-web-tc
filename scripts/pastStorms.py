0d9b800b10dc4e6b68829c06f9cde0fbc979ae48
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-02-27 08:20:46 +0000

    Update pastStorms.py

--- scripts/pastStorms.py
+++ scripts/pastStorms.py
@@ -13,19 +13,36 @@ dict_storms = {}
 for dir in dirs:
     for file in dict_files[dir]:
         if file.endswith('.geojson'):
-            dict_storms.setdefault(dir, {})
+            dict_storms.setdefault(dir, [])
             year = file.split('/')[1]
-            dict_storms[dir].setdefault(year, [])
             storm_id = file.split('_')[2]
-            if storm_id not in [s['id'] for s in dict_storms[dir][year]]:
-                rec = {'id': storm_id}
-                dict_storms[dir][year].append(rec)
-            i = [i for i, s in enumerate(dict_storms[dir][year]) if s['id'] == storm_id][0]
-            dict_storms[dir][year][i]['nc'] = file
+            if storm_id not in [s['id'] for s in dict_storms[dir]]:
+                rec = {'id': storm_id, 'year': year}
+                dict_storms[dir].append(rec)
+            i = [i for i, s in enumerate(dict_storms[dir]) if s['id'] == storm_id][0]
+            dict_storms[dir][i]['nc'] = file
             with open(file, 'r') as f:
                 data = json.load(f)
-            dict_storms[dir][year][i]['storm_name'] = data['storm']['name']
-            dict_storms[dir][year][i]['bbox'] = data['bbox']
+            dict_storms[dir][i]['storm_name'] = data['storm']['name']
+            dict_storms[dir][i]['bbox'] = data['bbox']
+
+
+# for dir in dirs:
+#     for file in dict_files[dir]:
+#         if file.endswith('.geojson'):
+#             dict_storms.setdefault(dir, {})
+#             year = file.split('/')[1]
+#             dict_storms[dir].setdefault(year, [])
+#             storm_id = file.split('_')[2]
+#             if storm_id not in [s['id'] for s in dict_storms[dir][year]]:
+#                 rec = {'id': storm_id}
+#                 dict_storms[dir][year].append(rec)
+#             i = [i for i, s in enumerate(dict_storms[dir][year]) if s['id'] == storm_id][0]
+#             dict_storms[dir][year][i]['nc'] = file
+#             with open(file, 'r') as f:
+#                 data = json.load(f)
+#             dict_storms[dir][year][i]['storm_name'] = data['storm']['name']
+#             dict_storms[dir][year][i]['bbox'] = data['bbox']
 
 with open('pastStorms.json', 'w') as f:
     json.dump(dict_storms, f, sort_keys=True, indent=4)
