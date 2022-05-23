7214ddb36aefac060e8651507fd20bd38ff85207
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-05-23 10:34:03 +0200

    Update pastStorms.py

--- scripts/pastStorms.py
+++ scripts/pastStorms.py
@@ -9,6 +9,44 @@ for dir in dirs:
     dict_files[dir] = glob.glob(f'{dir}/**/**.*', recursive=True)
 
 dict_storms = {}
+dict_storms_season = {}
+
+
+for dir in dirs:
+    for file in dict_files[dir]:
+        if file.endswith('.geojson'):
+            dict_storms_season.setdefault(dir, {})
+            year = file.split('/')[1]
+            dict_storms_season[dir].setdefault(year, [])
+            if file.endswith('SLOSH.geojson'):  # nc file
+                storm_id = file.split('_')[2]
+            else:
+                storm_id = file.split('/')[2].split('_')[0]
+            if storm_id not in [s['id'] for s in dict_storms_season[dir][year]]:
+                rec = {'id': storm_id, 'year': year}
+                dict_storms_season[dir][year].append(rec)
+            i = [i for i, s in enumerate(dict_storms_season[dir][year]) if s['id'] == storm_id][0]
+            if file.endswith('SLOSH.geojson'):  # nc file
+                dict_storms_season[dir][year][i]['nc'] = file
+                with open(file, 'r') as f:
+                    data = json.load(f)
+                dict_storms_season[dir][year][i]['storm_name'] = data['storm']['name']
+                dict_storms_season[dir][year][i]['bbox'] = data['bbox']
+                loss_file = f'{os.path.dirname(file)}/{storm_id}_losses_adm.json'
+                if os.path.exists(loss_file):
+                    dict_storms_season[dir][year][i]['losses'] = loss_file
+                else:
+                    dict_storms_season[dir][year][i]['losses'] = ''
+            else:
+                with open(file, 'r') as f:
+                    data = json.load(f)
+                date, time = min([f['properties']['DTG'] for f in data['features'] if f['properties']['DTG'] != '']).split(' ')
+                dict_storms_season[dir][year][i]['shp'] = file
+                dict_storms_season[dir][year][i]['date'] = date
+                dict_storms_season[dir][year][i]['time'] = time
+
+with open('pastStormsSeason.json', 'w') as f:
+    json.dump(dict_storms_season, f, sort_keys=True, indent=4)
 
 
 for dir in dirs:
@@ -59,5 +97,4 @@ for dir in dirs:
 
 with open('pastStorms.json', 'w') as f:
     json.dump(dict_storms, f, sort_keys=True, indent=4)
-
 
