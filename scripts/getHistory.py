31f0b7746a531f589d60fe8746912639c8dc2702
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2022-05-30 13:49:13 +0700

    Update getHistory.py

--- scripts/getHistory.py
+++ scripts/getHistory.py
@@ -29,8 +29,8 @@ def checkIfKeyAddValue(d, year, value, field='loss'):
 def getAllStorms(json_storms):
     list_past = glob.glob('jtwc_history/**/**_shp.geojson')
     list_latest = glob.glob('mpres_data/postevent/taos_swio30s_ofcl_windwater_shp/**.geojson')
-    dict_storms = {}
-    dict_storms['records'] = [{'adm0' : {'storms': list_past + list_latest}}]
+    dict_storms = {'records': []}
+    dict_storms['records'][0] = {'storms': list_past + list_latest}
     dict_storms['counter_total'] = len(set(list_past + list_latest))
 
     with open(json_storms, 'w') as f:
